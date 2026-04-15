525f72f98a2b53091550d1b84b83cb2543bd429b
Yashita Bahrani <bahraniyashita@gmail.com>
2026-04-16 03:23:15 +0530

    updated returns route file

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -1,12 +1,13 @@
 export const dynamic = "force-dynamic";
-import { NextResponse } from "next/server";
+
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } }
+  _request: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await context.params;
 
   const order = await prisma.order.findUnique({
     where: { id },
